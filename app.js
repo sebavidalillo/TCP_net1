6045644c95a18903670ff47d4346431867e70741
Seba Vidal <sebastian.f.vidal@gmail.com>
2023-04-12 13:48:35 -0400

    actualizacion de archivos al 12 de abril: añadir json de flespi 21 marzo, parsegtcan, idea internet, packagelockjson

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const net = require('net'); 
 var server = net.createServer(); 
 
-port = process.env.PORT || 3500; 
+port = process.env.PORT || 3000; 
 ip = 'my_host'; 
 
 server.on("connection", (socket) => { 
   console.log("new client connection is made", socket.remoteAddress + ":" + socket.remotePort); 
+  
+  // SOCKET.ON DATA //
   socket.on("data", (data) => { 
-    console.log(data.toString()); 
+    console.log(`Datos recibidos: ${data}`);
+
+    const ReporteSeparadoPorComas = data.toString().split(',');
+    const messages = GetMessages(ReporteSeparadoPorComas);  
+    console.log(messages); 
   }); 
+
   socket.once("close", () => { 
     console.log("client connection closed."); 
   }); 
@@ -17,6 +24,10 @@ server.on("connection", (socket) => {
   }); 
   socket.write('SERVER: Hello! Connection successfully made.<br>'); 
 }); 
+
+
+
+
 server.on('error', (e) => { 
   if (e.code === 'EADDRINUSE') { 
     console.log('Address in use, retrying...'); 
@@ -33,3 +44,58 @@ server.on('error', (e) => {
 server.listen(port, ()=>{
   console.log('sevidor corriendo en ', port); 
 });
+
+function GetMessages(ReporteSeparadoPorComas){ 
+    switch(ReporteSeparadoPorComas[0]){
+      case '+RESP:GTPNA': 
+        // rutina de ordenamiento en objetos para GTPNA; 
+        break; 
+      case '+RESP:GTFRI':
+        // rutina de asignación de campos para GTFRI;
+        return RESP_GTRI(ReporteSeparadoPorComas); 
+        break;
+      case '+RESP:GTCAN':
+        //rutina de asignación de campos para GTCAN; 
+        break; 
+    }
+}
+
+
+function RESP_GTRI(ReporteSeparadoPorComas){
+    const mensajes = {
+      IMEI: ReporteSeparadoPorComas[2], 
+      ReportMask: ReporteSeparadoPorComas[6],
+      Altitude: ReporteSeparadoPorComas[10], 
+      Longitud: ReporteSeparadoPorComas[11], 
+      Latitude: ReporteSeparadoPorComas[12], 
+      BackUpBattery: ReporteSeparadoPorComas[23], 
+      SendTime: ReporteSeparadoPorComas[28],
+
+    };
+    return mensajes; 
+}
+
+function RESP_GTCAN(ReporteSeparadoPorComas){
+  const mensajes = {
+    IMEI: ReporteSeparadoPorComas[2],
+    CanState: ReporteSeparadoPorComas[5], // 1 OK, 0 ABNORMAL
+    
+  };
+  return mensajes; 
+}
+
+
+// const objeto = {
+//   RESP: ReporteSeparadoPorComas[0],
+//   id: ReporteSeparadoPorComas[1],
+//   imei: ReporteSeparadoPorComas[2],
+//   velocidad: ReporteSeparadoPorComas[5],
+//   latitud: ReporteSeparadoPorComas[11],
+//   longitud: ReporteSeparadoPorComas[12],
+//   fecha: ReporteSeparadoPorComas[13],
+//   ignicion: ReporteSeparadoPorComas[24],
+//   bateria: ReporteSeparadoPorComas[25],
+//   timestamp: ReporteSeparadoPorComas[28]
+// };
+
+
--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 const net = require('net'); 
+const readLine= require('readline-sync'); 
+
 const options= {
-    port: 3500, 
+    port: 3000, 
     host: 'localhost'
 }
 
@@ -8,9 +10,17 @@ const client = net.createConnection(options);
 
 client.on('connect', ()=>{
     console.log('conexion exitosa'); 
-    client.write('Hola servidor'); 
+    sendLine(); 
+    //client.write('+RESP:GTFRI,270F01,863457051459871,,,10,1,1,,,70.3,-71.602106,-33.045132,20230405203914,,,,,00,,,,,0,110000,,,,20230405203924,499C$'); 
+    //client.write('+RESP:GTCAN,270F01,863457051459871,,0,1,C0000134,H1227320,660,0,L17.00,,,0,0.0,0,67.4,-71.557849,-33.027922,20230321153602,0730,0002,6979,235CC8,00,20230321153604,47C8$')
+    //client.write('+RESP:GTCAN,270F01,863457051459871,,0,1,A07FFFFF,,2,H1222390,0.39,645,0,95,,L9.50,,0,0.35,0.04,0.31,0.28,,,0042,,00,,,001FFFFF,,,,,,,,,69,102,0,0.04,,0.00,2,,,,,,0000,,,0730,0002,6979,232EA9,00,20230309150847,2C96$') 
 })
 
 client.on('error', (err)=>{
     console.log(err.message); 
 })
+
+function sendLine(){
+    const line = readLine.question('\n mensaje a enviar:\t');
+    client.write(line); 
+}
